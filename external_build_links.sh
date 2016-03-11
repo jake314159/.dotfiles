294981bfa8a9b802a6ee0c4767f8960d0d252c43
Jake Causon <jake.causon@freshrelevance.com>
2016-03-11 10:21:37 +0000

    Store remote repos in own dir

--- external_build_links.sh
+++ external_build_links.sh
@@ -2,19 +2,29 @@
 
 #
 # Download, link and update external scripts from other git repos
+# Copy of each remote repo is stored in ./remote/ and linked to from ~/bin
 #
 
 DIR=`pwd`
+
+if [ ! -d "./remote" ]; then
+    echo "BUILD: Adding remote directory"
+    mkdir "remote"
+fi
+
+cd "./remote"
 
 # prettyjson
 if [ -d "./prettyjson" ]; then
     echo "BUILD: Updating prettyjson"
     cd "./prettyjson"
     git pull
-    cd "$DIR"
+    cd "$DIR/remote"
 else
     echo "BUILD: Cloning prettyjson"
     git clone git@github.com:jake314159/prettyjson.git
     rm ~/bin/prettyjson
-    ln -s "$DIR/prettyjson/src/prettyjson" ~/bin/prettyjson
+    ln -s "$DIR/remote/prettyjson/src/prettyjson" ~/bin/prettyjson
 fi
+
+cd "$DIR"
